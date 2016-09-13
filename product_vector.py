d7ad53771921d691245091664ca1ce0561597b62
murilosandiego <murilosandiego1@gmail.com>
2016-09-13 18:55:54 -0300

    Implementa enunciado das questões

--- product_vector.py
+++ product_vector.py
@@ -1,3 +1,15 @@
+"""
+Você está resolvendo este problema. 
+Este problema foi utilizado em 111 Dojo(s).
+Definimos dois vetores A e B de dimensão n em termos de componentes como:
+A = (a1, a2, a3, ..., an) e B = (b1, b2, b3, ..., bn)
+O produto escalar entre esses vetores é descrito como:
+A . B = a1 * b1 + a2 * b2 + a3 * b3 + ... + an * bn
+Desenvolva um programa que, dado dois vetores de dimensão n, retorne o produto escalar entre eles.
+"""
+
+
+
 def calc(a, b):
     soma = 0
     for i in range(0,len(a)):
